5b8ddc87b6fa47253edfc830598bc9dc5e54bcf3
Jan Hug <me@dulnan.net>
2023-01-04 09:13:51 +0100

    Generate types in build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  plugins: [
+    dts({
+      insertTypesEntry: true
+    })
+  ],
   build: {
+    outDir: 'lib',
     lib: {
       // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'lib/main.ts'),
+      entry: resolve(__dirname, 'src/main.ts'),
       name: 'lazy-brush',
       // the proper extensions will be added
       fileName: 'lazy-brush'
