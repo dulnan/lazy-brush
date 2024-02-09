9e094ef1acb4e404dc994de12142c47357fea5c6
joqqy <realbrush@gmail.com>
2024-02-10 00:00:51 +0100

    Midpoint, calculation simplification.

--- demo/components/Scene.vue
+++ demo/components/Scene.vue
@@ -237,8 +237,8 @@ function loop() {
 
 function midPointBtw(p1: Point, p2: Point) {
   return {
-    x: p1.x + (p2.x - p1.x) / 2,
-    y: p1.y + (p2.y - p1.y) / 2
+    x: (p2.x + p1.x) / 2,
+    y: (p2.y + p1.y) / 2
   }
 }
 
